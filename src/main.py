6dd36677d71ebd36134b2b1bc82d77e887d6a75d
TylerJones429 <TylerJones4292@gmail.com>
2024-12-10 20:19:11 -0500

    created test_textnode, suffered

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from textnode import TextNode, TextType
 
+
 def main():
-    test_node = TextNode("This is a test node", TextType.BOLD, "www.boot.dev")
-    print (test_node)
+    node = TextNode("This is a test node", TextType.BOLD, "www.boot.dev")
+    print (node)
+
 
 main()
